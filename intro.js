b99edb3c1122b1088ef8149ce89f89e6e6dff2cd
MorganTabor <146842116+MorganTabor@users.noreply.github.com>
2025-07-02 22:43:31 +0000

    Horrible mess of baby's first javascrips

---
+++ intro.js
@@ -0,0 +1,20 @@
+console.log("Hewwo?")
+
+const Ouhf = 2313
+let therebe = "light"
+
+var hagbag = "HoooooooooooooooooooWee"
+baghag = "WeeHooooooo000"
+let snaggledy;
+
+console.log("weedoo" + snaggledy);
+ 
+console.log(`Hogbottom ${hagbag} babblededee`)
+
+console.log(3+3)
+
+console.log(typeof(undefined))
+console.log(typeof(null))
+
+let randomstring = "21fs"
+console.log(Number(randomstring))
